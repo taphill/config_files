bfe81f71c6384d11b9cf3d64421f7dba344405a2
Taylor Phillips <taylorscottphillips@gmail.com>
2021-03-04 19:49:57 -0600

    Update plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,5 +1,3 @@
---NOTE: Packages are in the runtimepath, this file is only loaded for updates.
-
 vim.cmd 'packadd paq-nvim'         -- Load package
 local paq = require'paq-nvim'.paq  -- Import module and bind `paq` function
 paq{'savq/paq-nvim', opt=true}     -- Let Paq manage itself
@@ -9,6 +7,9 @@ paq 'kyazdani42/nvim-tree.lua'
 
 paq 'nvim-lua/popup.nvim'
 paq 'nvim-lua/plenary.nvim'
---paq 'romgrk/fzy-lua-native'
 paq 'nvim-telescope/telescope.nvim' --uses popup & plenary
---paq 'nvim-telescope/telescope-fzy-native.nvim'
+
+paq 'nvim-treesitter/nvim-treesitter'
+paq 'nvim-treesitter/playground'
+
+paq 'terrortylor/nvim-comment'
