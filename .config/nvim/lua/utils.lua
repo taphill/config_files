6454d46652c9ad5baeb65375e6ae6ed3a34e536f
Taylor Phillips <taylorscottphillips@gmail.com>
2021-03-04 19:50:18 -0600

    Update utils

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,13 +1,5 @@
 -- Wait for lua keymaps (nvim PR #13823)
 
---local function map(lhs, rhs, mode)
- -- vim.api.nvim_set_keymap(
-  --  mode or 'n',
-   -- lhs,
-    --'<cmd>' .. rhs .. '<cr>',
-  --  {noremap=true, silent=true}
- -- )
---end
 local a = vim.api
 
 local function map(mod, lhs, rhs, opt)
