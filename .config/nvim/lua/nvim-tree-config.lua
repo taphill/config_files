f05780a1b5493d966c37854ade0ccd84dd20a44c
Taylor Phillips <taylorscottphillips@gmail.com>
2021-03-13 12:52:42 -0600

    Update nvim-tree config

--- .config/nvim/lua/nvim-tree-config.lua
+++ .config/nvim/lua/nvim-tree-config.lua
@@ -1,9 +1,19 @@
 local g = vim.g
 
-g.nvim_tree_auto_open = 0 --0 by default, opens the tree when typing `vim $DIR` or `vim`
-g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
-g.nvim_tree_disable_netrw = 0 --1 by default, disables netrw
-g.nvim_tree_hijack_netrw = 0 --1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
+g.nvim_tree_side = 'left' -- left by default
+g.nvim_tree_width = 30 -- 30 by default
+g.nvim_tree_auto_open = 1 -- 0 by default, opens the tree when typing `vim $DIR` or `vim`
+g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
+g.nvim_tree_auto_ignore_ft = {} -- empty by default, don't auto open tree on specific filetypes.
+g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
+g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
+g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
+g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
+g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).nvim_tree_root_folder_modifier = ':~' "This is the default. See :help filename-modifiers for more options
+g.nvim_tree_tab_open = 1 -- 0 by default, will open the tree when entering a new tab and the tree was previously open
+g.nvim_tree_width_allow_resize  = 1 -- 0 by default, will not resize the tree when opening a file
+g.nvim_tree_disable_netrw = 0 -- 1 by default, disables netrw
+g.nvim_tree_hijack_netrw = 0 -- 1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
 
 g.nvim_tree_icons = {
   default = '',
